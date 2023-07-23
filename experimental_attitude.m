d01fc52cfaefe4e3fa82272fd647009b39cb94a4
An Dang <dangthanhan507@gmail.com>
2023-07-23 16:52:53 -0700

    Created preliminary file for testing

---
+++ experimental_attitude.m
@@ -0,0 +1,15 @@
+% ============================================================
+% experimental_attitude.m
+% 
+% Experimental code to try and get attitude dynamics into
+% MPC-MHE. Currently closely followign this paper.
+% https://arxiv.org/pdf/2211.11653.pdf
+% 
+% Author: An Dang
+%
+% ============================================================
+
+
+
+
+disp("Hello World\n")
