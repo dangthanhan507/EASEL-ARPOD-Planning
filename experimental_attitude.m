940c18ba38ab354fe81500f4e36fe3ee90717172
An Dang <dangthanhan507@gmail.com>
2023-07-23 17:34:53 -0700

    Added needed comments

--- experimental_attitude.m
+++ experimental_attitude.m
@@ -9,6 +9,30 @@
 %
 % ============================================================
 
+%{
+    NOTES:
+
+    Dynamics are the same as HCW but it now also adds Rotation to the thrusters
+
+    In other words:
+    ---------------
+    statedot = A*state + Rot*B*u
+
+    where Rot is SO(3)
+
+
+    using MATLAB's zoh will be very useful.
+
+    seems like having attitude be supported should stay within 3d.
+
+    QUESTION: decouple translation and rotation??
+
+    seems like the rotation is based on our pose relative to the target spacecraft.
+
+    in this case, we jus include a rotation matrix as usual.
+
+    when thruster misalignment happens, we induce a change in the rotation matrix
+%}
 
 
 
