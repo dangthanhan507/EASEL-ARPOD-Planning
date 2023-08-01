4b590a5c0145a822715b3dab171c877afeedb262
An Dang <dangthanhan507@gmail.com>
2023-07-31 18:23:37 -0700

    Added mpc-pytorch implementation

--- python/eval.py
+++ python/eval.py
@@ -3,38 +3,63 @@ from torch.autograd import Variable
 from mpc import mpc
 from mpc.mpc import QuadCost, LinDx
 
+import torch.nn as nn
+class SimpleDynamicsNetwork(nn.Module):
+    def __init__(self):
+        super().__init__() #must call
+
+        #setting up hidden layer
+
+        # control input is 3, state input is 6... that makes 9
+        self.layers = []
+        self.layers.append(nn.Linear(9,64,bias=True))
+        self.layers.append(nn.Linear(64,128,bias=True))
+        self.layers.append(nn.Linear(128,256,bias=True))
+        self.layers.append(nn.Linear(256,256,bias=True))
+        self.layers.append(nn.Linear(256,256,bias=True))
+        self.layers.append(nn.Linear(256,128,bias=True))
+        self.layers.append(nn.Linear(128,64,bias=True))
+        self.layers.append(nn.Linear(64,6,bias=True))
+        self.model = nn.Sequential(*self.layers)
+    
+    def forward(self, x,u):
+        '''
+        '''
+        inp = torch.hstack((x,u))
+        return self.model(inp)
 
 if __name__ == '__main__':
     torch.manual_seed(0)
 
-    n_batch, n_state, n_ctrl, T = 2, 3, 4, 5
+    device = 'cpu'
+    network = SimpleDynamicsNetwork()
+    network_load = torch.load('./models/hcw_simple_model.pt').to(device)
+    network.load_state_dict(network_load.state_dict())
+
+    n_batch, n_state, n_ctrl, T = 1, 6, 3, 10
     n_sc = n_state + n_ctrl
 
-    # Randomly initialize a PSD quadratic cost and linear dynamics.
+    #load network
+
+    # Randomly initialize a PSD quadratic cost
     C = torch.randn(T*n_batch, n_sc, n_sc)
     C = torch.bmm(C, C.transpose(1, 2)).view(T, n_batch, n_sc, n_sc)
     c = torch.randn(T, n_batch, n_sc)
 
-    alpha = 0.2
-    R = (torch.eye(n_state)+alpha*torch.randn(n_state, n_state)).repeat(T, n_batch, 1, 1)
-    S = torch.randn(T, n_batch, n_state, n_ctrl)
-    F = torch.cat((R, S), dim=3)
-
     # The initial state.
-    x_init = torch.randn(n_batch, n_state)
-
-    # The upper and lower control bounds.
-    u_lower = -torch.rand(T, n_batch, n_ctrl)
-    u_upper = torch.rand(T, n_batch, n_ctrl)
-
-    x_lqr, u_lqr, objs_lqr = mpc.MPC(
-        n_state=n_state,
-        n_ctrl=n_ctrl,
-        T=T,
-        u_lower=u_lower, 
-        u_upper=u_upper,
-        lqr_iter=20,
-        verbose=1,
-        backprop=False,
-        exit_unconverged=False,
-    )(x_init, QuadCost(C, c), LinDx(F))
+    n_batch = 1
+    x_init = torch.Tensor([[10,10,10,0,0,0]]) #1,6
+    ctrl_mpc = mpc.MPC(n_state,
+                        n_ctrl,
+                        T,
+                        u_lower=-0.1*torch.ones(T,n_batch,n_ctrl),
+                        u_upper=0.1*torch.ones(T,n_batch,n_ctrl),
+                        lqr_iter=20,
+                        verbose=1,
+                        backprop=False,
+                        exit_unconverged=False,
+                        grad_method=mpc.GradMethods.AUTO_DIFF)
+    
+    xs, us, objs = ctrl_mpc(x_init, QuadCost(C,c), network)
+    print(xs)
+    
